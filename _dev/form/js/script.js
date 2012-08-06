ebeff44c580702d3f8408faa76a87b198705016a
syraz37 <syraz37@gmail.com>
2012-08-06 17:42:10 +0545

    form on progress

--- _dev/form/js/script.js
+++ _dev/form/js/script.js
@@ -1,6 +1,13 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
-
+jQuery.fn.togForm = function() {
+    var form = $(this[0]) // It's your element
+    var lines = form.html().split("\n");
+    $.each(lines, function(n, elem) {
+        elem = $.trim(elem);
+        console.log(elem);
+        if(elem != '')
+            {
+                //form.html().('<div class="new" />');
+            }
+    });
+};
