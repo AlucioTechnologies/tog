fe86eb051e4a0110843ba435a258d1c3613ef4c3
adishaswot <adishaswot@gmail.com>
2012-08-06 17:42:04 +0545

    checkBox basic functionality done

--- _dev/checkbox/js/script.js
+++ _dev/checkbox/js/script.js
@@ -1,6 +1,29 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/*Custom CheckBox */
 
+//Hide Current Checkbox
+$(".togCheckBox").hide();
 
+//Add a replacement for custom checkBox
+$(".togCheckBox").after("<div class='togCheckBoxReplacement'></<div>")
+
+//Attach click Functionalily
+$(".togCheckBoxReplacement").live("click",function(){
+      
+    //Check if the btn is checked previously
+    if($(".togCheckBoxReplacement").attr("checked")=="checked")
+        {
+        
+            $(".togCheckBoxReplacement").removeAttr("checked");
+            $(".togCheckBoxReplacement").removeClass("togCheckBoxReplacementHvr");
+            
+        }
+        else
+            {
+    
+   $(".togCheckBoxReplacement").attr("checked","checked");
+   
+    $(".togCheckBoxReplacement").addClass("togCheckBoxReplacementHvr");
+            }
+    
+  
+});
