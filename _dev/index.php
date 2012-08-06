20bc6f95c2c168a9004e3e0b5b039a5f90ee8839
syraz37 <syraz37@gmail.com>
2012-08-06 16:49:03 +0545

    new

---
+++ _dev/index.php
@@ -0,0 +1,10 @@
+<form name="compose_form" id="compose_form" >
+                
+				Send to:<input type="text" name="send_to" id="send_to" value=""/><br/>
+                
+				Subject:<input type="text" name="subject" id="subject" value=""/><br/>
+                
+				Message:<textarea name="message"></textarea><br/>
+				
+                <input type="submit" name="btn_send_msg" value="SEND" class="napzap_button" /><br/>
+</form>
