1abe95ce8af386233042e6f94c1e947ca2ade54c
syraz37 <syraz37@gmail.com>
2012-08-07 11:56:09 +0545

    form done

--- _dev/form/js/script.js
+++ _dev/form/js/script.js
@@ -2,12 +2,22 @@
 jQuery.fn.togForm = function() {
     var form = $(this[0]) // It's your element
     var lines = form.html().split("\n");
+    form.html('<ul></ul>')
     $.each(lines, function(n, elem) {
+        
         elem = $.trim(elem);
-        console.log(elem);
+        
         if(elem != '')
-            {
-                //form.html().('<div class="new" />');
-            }
+        {
+            elem = elem.replace('<br>', '');
+            elem = elem.replace('<br/>', '');
+            form.children("ul").append('<li>' + elem + '</li>');
+        }
+        
+    });
+    
+    form.children("ul").children("li").each(function(n, elem) {
+        text = $(this).text();
+        $(this).html($(this).html().replace(text,'<label>'+text+'</label>'));
     });
 };
